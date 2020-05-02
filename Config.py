1b60f1b697bc06151b0d2989b040fb77c9a46f11
kozhevnykov <oleksandr.kozhevnykov@cognite.com>
2020-05-02 13:07:04 +0300

    Привел в порядок, отфарматировал

---
+++ Config.py
@@ -0,0 +1,20 @@
+import telebot
+import json
+
+with open("credentials/botkeys.json") as file:
+    credentials = json.loads(file.read())
+
+bot = telebot.TeleBot("1086481184:" + credentials["botkey"]["1086481184"])
+Number = []
+queue = {"Food": {"3": "Roma", "1": "Food"}, "Spa": {"3": "Sasha"}}
+Shops = {"Food": "123", "Spa": "22"}
+choose_shop = ""
+Users = {"Roma": "1233", "Sasha": "2"}
+myQueue = {"Roma": {"Food": "3"}, "Sasha": {"Food": 1, "Spa": 3}}
+Carma = {"Roma": 10}
+Chat_shops = {}
+Chat_clients = {}
+flag = "start"
+type_user = ""
+user_login = ""
+max_number_clients = 10
--- Main.py
+++ Main.py
@@ -1,22 +1,43 @@
 import telebot
+
 # from telebot import apihelper
 # apihelper.proxy = {'https':'socks5://userproxy:password@proxy_address:port'}
 import numpy as np
 
-from Options import bot,Number,queue,Shops,choose_shop,Users,myQueue,Chat_shops,Chat_clients,flag,type_user,user_login
-from Controller import send_text_controller,start_message_controller,help_message_controller
+from Config import (
+    bot,
+    Number,
+    queue,
+    Shops,
+    choose_shop,
+    Users,
+    myQueue,
+    Chat_shops,
+    Chat_clients,
+    flag,
+    type_user,
+    user_login,
+)
+from Controller import (
+    send_text_controller,
+    start_message_controller,
+    help_message_controller,
+)
 
-@bot.message_handler(commands=['help'])
+
+@bot.message_handler(commands=["help"])
 def help_message(message):
     help_message_controller(message)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start_message(message):
     start_message_controller(message)
 
-@bot.message_handler(content_types='text')
+
+@bot.message_handler(content_types="text")
 def send_text(message):
     send_text_controller(message)
+
 
 bot.polling()
