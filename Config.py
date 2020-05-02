8a8f8ebac7c33d298a11ec2eba8d959d6ac09ca4
kozhevnykov <oleksandr.kozhevnykov@cognite.com>
2020-05-02 13:07:04 +0300

    Added client functionality

--- Config.py
+++ Config.py
@@ -1,13 +1,15 @@
 import telebot
+import json
 
 bot = telebot.TeleBot('1086481184:AAE0h7NBROzRO7Ke2QmTd7qPOYGy3WTMCM0')
 Number = []
-queue = {'Food': {'3': 'Roma',"1":"Food"}, "Spa":{"3":"Sasha"}}
-Shops = {"Food":"123","Spa":"22"}
+queue = {"Food": [],"Spa":[]}
+Shops = ["Food","Spa"]
+
 choose_shop = ""
-Users = {"Roma":"1233","Sasha":"2"}
-myQueue = {"Roma":{"Food":"3"},"Sasha":{"Food":1,"Spa":3}}
-Carma = {"Roma":10}
+Users = []
+myQueue = {}
+Carma = {}
 Chat_shops = {}
 Chat_clients = {}
 flag = 'start'
--- Controller.py
+++ Controller.py
@@ -1,203 +1,170 @@
 import telebot
-from Options import Carma,max_number_clients,bot,Number,queue,Shops,choose_shop,Users,myQueue,Chat_shops,Chat_clients,flag,type_user,user_login
+from Options import Carma, max_number_clients, bot, Number, queue, Shops, choose_shop, Users, myQueue, Chat_shops, \
+    Chat_clients, flag, type_user, user_login
+
 
 def send_text_controller(message):
     User_name = "мой друг"
     global flag
-    print(f'Флаг в начале отправки {flag}')
-    Comands = {'привет':"start",'пока':"finish", "клиент":'client','магазин':'shop',
-               "войти в аккаунт":"login","cоздать новый":"","какой я в очереди":"my_position",
-               "выбрать магазин":"choice_shop","отменить запись":"cancel_record","выйти": "finish"
-            }
+    Comands = {"стать в очередь": "get_talon", "отменить запись": "cancel_talon", "какой я в очереди": "my_position",
+               "выйти": "exit","cписок магазинов":"show_shop_list",
+               "позвать следующего": "next_client", "первые 3 клиента": "head_queue", "очистить очередь": "clean_queue",
+               "удалить магазин": "delete_shop"
+               }
     if message.text.lower() in Comands:
         flag = Comands[message.text.lower()]
-    flag = answer_maker(User_name, message.chat.id, message.text)
-    if flag == "work":
-        answer_maker(User_name, message.chat.id, message.text)
-    print(f"флаг после {flag} \n {Users} {Shops} {queue}")
+    print(f"До ответа flag :{flag} message:{message.text} Users: {Users} Shops:{Shops} queue:{queue} myQueue:{myQueue}")
+    flag = answer_maker(message.chat.id, message.text.lower())
+
+    print(f"После ответа flag :{flag} message:{message.text}  Users: {Users} Shops:{Shops} queue:{queue} myQueue:{myQueue}")
+
 
 def start_message_controller(message):
-    global flag,type_user,user_login,Users,Shops
-    _id = message.chat.id
-    bot.send_message(message.chat.id, f'Привет, напиши \n /start чтобы начать\n /help чтобы узнать список компанд')
-    User_name = ""
+    global flag, type_user, user_login, Users, Shops
     flag = "start"
-    _id = message.chat.id
-    if not User_name:
-        User_name = "мой друг"
-    answer_maker(User_name, message.chat.id, message = message)
+    send_text_controller(message)
 
 def help_message_controller(message):
-    bot.send_message(message.chat.id, f'Привет, напиши: \n/start чтобы авторизироваться\n/help чтобы узнать список компанд \n"Пока" для выхода \n/slavaUkraine')
+    bot.send_message(message.chat.id,
+                     f'Привет, напиши: \n/start чтобы авторизироваться\n/help чтобы узнать список компанд \n"Пока" для выхода')
 
-def answer_maker( User_name, _id, message=None):
+
+def answer_maker(_id, message=None):
+    global flag, type_user, user_login, Users, Shops,queue,myQueue
     try:
-        keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-        global flag,type_user,user_login,Users,Shops,Chat_shops,Chat_clients,queue,choose_shop,Carma
-        print(f'Флаг в начале {flag}')
-        if flag == "work":
-             keyboard1.row("Выбрать магазин", "Отменить запись", "Какой я в очереди", "Выйти")
-             bot.send_message(_id, "Что хотите сделать дальше", reply_markup=keyboard1)
-        if flag=="start":
+        if flag == "start":
+            keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
             keyboard1.row("Клиент", "Магазин")
-            bot.send_message(_id, "Выбери свою роль",reply_markup=keyboard1)
+            bot.send_message(_id, "Выбери свою роль", reply_markup=keyboard1)
             return "wait_type"
-        if flag=="finish":
-            bot.send_message(_id, f'Прощай, {User_name}')
-            bot.send_message(_id, f'напиши /start чтобы начать')
-            user_login =""
+        if flag == "exit":
             type_user = ""
-            return "start"
-        if flag == 'client' or flag=='shop':
-            keyboard1.row("Войти в аккаунт", "Создать новый")
-            bot.send_message(_id, f"Вы выбрали {flag}")
-            type_user = flag
-            bot.send_message(_id, "Что хотите сделать дальше", reply_markup=keyboard1)
-            return "wait_type_aut"
-        if flag == "wait_type_aut":
-            if message=="Войти в аккаунт":
-                flag='login'
-            elif message=="Создать новый":
-                flag="create_acc"
-            else:
-                flag = type_user
-        if flag=="create_acc":
-            bot.send_message(_id, 'Придумай логин')
-            return "wait_login_reg"
-        if flag == 'wait_login_reg':
-            return new_acc(_id,message)
-        if flag == "wait_password":
-            if type_user=="client":
-                Users[user_login] = message
-                myQueue[user_login]={}
-                Carma[user_login] = 10
-                Chat_clients[user_login]=_id
-            else:
-                Shops[user_login] = message
-                queue[user_login] = {}
-                Chat_shops[user_login]=_id
-            bot.send_message(_id, 'Принято, теперь можно работать')
-            return "work"
-        if flag=="login":
-            bot.send_message(_id, 'Введите Ваш логин')
-            return "wait_login_aut"
-        if flag=="wait_login_aut":
-            user_login = message
-            if user_login not in Users:
-                bot.send_message(_id, 'Введите Ваш настоящий логин')
-                return "wait_login_aut"
-            bot.send_message(_id, 'Введите Ваш пароль')
-            return "wait_password_log"
-        if flag =="wait_password_log":
-            return login(_id, message)
-        if flag == "my_position":
-            if user_login not in myQueue:
-                bot.send_message(_id,"Вы еще никуда не записались")
-            else:
-                bot.send_message(_id,f"Ваши записи:\n {myQueue[user_login]} ")
-            return "work"
-        if flag == "cancel_record":
+            flag = "start"
+            return answer_maker(_id,"")
+        if flag == "wait_type":
+            if message == "клиент":
+                Users.append(_id)
+                type_user = "client"
+                myQueue[_id] = {}
+                flag = "home"
 
-            if (user_login not in myQueue) or (len(myQueue[user_login].keys())) == 0:
-                bot.send_message(_id,"Вы еще никуда не записались")
+                return answer_maker(_id, message)
+            elif message == "магазин":
+                Shops.append(_id)
+                queue[_id] = {}
+                type_user = "shop"
+                flag = "home"
+                return answer_maker(_id, message)
             else:
-                bot.send_message(_id, f"Из какого магазина Вас убрать: {myQueue[user_login].keys()}")
-            return "wait_choice_cancel"
-        if flag == "wait_choice_cancel":
-            print(f"удаляю из {user_login}, {queue}, {queue[message]}")
-            if get_key(queue[message],user_login):
-                print("удаляю")
-                del queue[message][get_key(queue[message],user_login)]
-                del myQueue[user_login][message]
-                bot.send_message(_id, f"Вы больше не записаны в {message} ")
+                bot.send_message(_id, "Неверный ввод")
+                flag = "start"
+                return answer_maker(_id, message)
+        if flag == "show_shop_list":
+            printShopList(_id)
+            flag = "home"
+            return answer_maker(_id,"")
+        if flag == "home":
+            keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+            if type_user == "client":
+                keyboard1.row("Cписок магазинов","Стать в очередь", "Отменить запись", "Какой я в очереди", "Выйти")
+                bot.send_message(_id, "Выбери действие", reply_markup=keyboard1)
+            elif type_user == "shop":
+                keyboard1.row("Позвать следующего", "Первые 3 клиента", "Очистить очередь", "Удалить магазин", "Выйти")
+                bot.send_message(_id, "Выбери действие", reply_markup=keyboard1)
             else:
-                bot.send_message(_id, f"Вы больше не записаны в {message} ")
-            return "work"
-        if flag == "choice_shop":
-            bot.send_message(_id, f"Ловите список магазинов, в какой записать Вас?: {[Shops.keys()]}")
-            return "wait_choice_shop"
-        if flag == "wait_choice_shop":
-            if message in queue.keys():
-                 if message in myQueue[user_login]:
-                     bot.send_message(_id,f'Вы уже записаны в {message},\nВаша очередь там {myQueue[user_login][message]}')
-                     return "work"
-                 bot.send_message(_id,f'свободны такие номера{freeNum(message)}')
-                 choose_shop = message
-                 return "wait_number_queue"
-            elif message not in Shops:
-                bot.send_message(_id, f'Нет такого магазина')
-                return "choice_shop"
+                flag = "start"
+                return answer_maker(_id, message)
+        if flag == "get_talon":
+            bot.send_message(_id, "Выбери магазин в котором встать в очередь\n")
+            printShopList(_id)
+            return "wait_shop_num"
+        if flag == "wait_shop_num":
+            try:
+                num = int(message)
+                return addToQueue(_id, num - 1)
+            except Exception as e:
+                print("numer_error")
+                bot.send_message(_id, "Введите корректный номер")
+                bot.send_message(_id, "Выбери магазин в котором встать в очередь\n")
+                printShopList(_id)
+                return "wait_shop_num"
+        if flag == "cancel_talon":
+            if myQueue[_id]=={}:
+                bot.send_message(_id, "У вас нет записей")
+                flag = "home"
+                return answer_maker(_id,message)
             else:
-                bot.send_message(_id,'Все номера свободный, введите на какой номер Вас записать')
-                choose_shop = message
-                return "wait_number_queue"
-        if flag == "wait_number_queue":
-            if not message.isdigit():
-                bot.send_message(_id,"введите число больше 0")
-            if int(message)<=0:
-                bot.send_message(_id,"введите число больше 0")
-            if choose_shop not in queue:
-                queue[choose_shop] = {}
-                queue[choose_shop][message] = user_login
-                if user_login not in myQueue:
-                    myQueue[user_login] = {}
-                myQueue[user_login][choose_shop] = message
-                bot.send_message(_id,f'Вы записаны в {choose_shop}, порядок в очереди {message}')
-            elif message in queue[choose_shop].keys():
-                bot.send_message(_id,f'Номер занят, посмотри еще\n {freeNum(choose_shop)}')
-                return "wait_number_queue"
-            else:
-                queue[choose_shop][message] = user_login
-                myQueue[user_login][choose_shop] = message
-                bot.send_message(_id,f'Вы записаны в {choose_shop}, порядок в очереди {message}')
-        return "work"
+                bot.send_message(_id,f"Выбери какой талон отменить\n{printMyTalons(_id)}")
+                return "wait_talon_del"
+        if flag =="wait_talon_del":
+            return delTalonByNumber(_id,message)
+        if flag == "my_position":
+            return getMyPosition(_id)
+
     except Exception as e:
         print("answer_maker_error")
-        bot.send_message(_id,"Попробуй еще раз выполнить последнюю команду")
+        bot.send_message(_id, "Попробуй выполнить команду еще раз")
+        return flag
 
 
-def new_acc(_id, login):
-    global flag,Users,Shops,user_login
-    if type_user == 'client':
-        if login in Users:
-            bot.send_message(_id, 'Ваш логин занят')
-            flag = "create_acc"
-        else:
-            bot.send_message(_id, 'Принято, теперь придумай пароль')
-            user_login = login
-            flag = "wait_password"
-    if type_user == 'shop':
-        if login in Shops:
-            bot.send_message(_id, 'Ваш логин занят')
-            flag = "wait_login_reg"
-        else:
-            bot.send_message(_id, 'Принято, теперь придумай пароль')
-            user_login = login
-            flag = "wait_password"
-    print(Users,flag)
-    return flag
-def login(_id,password):
-    global type_user,user_login,Users,Shops
-    if type_user=='client':
-        if Users[user_login]==password:
-            bot.send_message(_id, 'успешно вошли')
-            return "work"
-        else:
-            bot.send_message(_id, 'попробуй еще')
-            return "wait_password_log"
-    if type_user=='shop':
-        if Shops[user_login]==password:
-            bot.send_message(_id, 'успешно вошли')
-            return "work"
-        else:
-            bot.send_message(_id, 'попробуй еще')
-            return "wait_password_log"
+def addToQueue(id, num_in_shops):
+    global flag, type_user, user_login, Users, Shops, queue, myQueue
+    if Shops[num_in_shops] in myQueue[id]:
+        bot.send_message(id,f"Вы уже были добавлены в {Shops[num_in_shops]}, Ваш номер {myQueue[id][Shops[num_in_shops]]}")
+        flag = "home"
+        return answer_maker(id,"")
+    queue[Shops[num_in_shops]].append(id)
+    myQueue[id][Shops[num_in_shops]] = len(queue[Shops[num_in_shops]])
+    bot.send_message(id, f"Мы Вас записали в {Shops[num_in_shops]} на место {len(queue[Shops[num_in_shops]])}")
+    flag = "home"
+    return answer_maker(id, "")
+
+def printMyTalons(id):
+    global flag, type_user, user_login, Users, Shops, queue,myQueue
+    mes = ""
+    j = 0
+    for shop, num in myQueue[id].items():
+        j += 1
+        mes+=f"{j}: {shop} номер в очереди {num}\n"
+    return mes
+def delTalonByNumber(id,number):
+    global flag, type_user, user_login, Users, Shops, queue,myQueue
+    j = 0
+    for shop, num in myQueue[id].items():
+        j+=1
+        if number == str(j):
+            del myQueue[id][shop]
+            del queue[shop][queue[shop].index(id)]
+            bot.send_message(id, f"Ваш тало в {shop} был удален")
+            print(f"я тут 2")
+            flag = "home"
+            return answer_maker(id,"")
+    flag = "cancel_talon"
+    bot.send_message(id,"Ошибка ввода")
+    return answer_maker(id,"")
+def printShopList(id):
+    global flag, type_user, user_login, Users, Shops, queue, myQueue
+    j = 0
+    mes = ""
+    for i in Shops:
+        j += 1
+        mes = mes + f"{j}: {i}\n"
+    print(f"mes:{mes}")
+    bot.send_message(id, mes)
+def getMyPosition(id):
+    global flag, type_user, user_login, Users, Shops, queue, myQueue
+    if myQueue[id] == {}:
+        bot.send_message(id,f"У вас нет записей")
+        flag = "home"
+        return answer_maker(id, "")
+    else:
+        mes = ""
+        for shop,num in myQueue[id].items():
+            mes+= f"В магазине {shop} Ваша очередь {num}\n"
+        bot.send_message(id,mes)
+        flag = "home"
+        return answer_maker(id,"")
 def get_key(d, value):
     for k, v in d.items():
         if v == value:
             return k
-def freeNum(choose_shop):
-    global queue,max_number_clients
-    res = []
-    print(f'{queue[choose_shop].keys()}')
-    return list([set(range(1,max_number_clients))-set([int(item) for item in queue[choose_shop].keys()])])
